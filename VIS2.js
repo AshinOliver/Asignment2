079cc79e86dcb7c3a92adf8e4aff9cbaea06d551
AshinOliver <89186258+AshinOliver@users.noreply.github.com>
2021-10-13 16:58:23 +0800

    Add files via upload

---
+++ VIS2.js
@@ -0,0 +1,28 @@
+var vg_1 = "case_map.json";
+vegaEmbed('#map1', vg_1, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+var vg_2 = "vac_map.json";
+vegaEmbed('#map2', vg_2, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+
+var vg_3 = "bar.json";
+vegaEmbed('#map3', vg_3, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+
+var vg_4 = "scatter_ploy.json";
+vegaEmbed('#map4', vg_4, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+
+var vg_5 = "line_chart.json";
+vegaEmbed('#map5', vg_5, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
